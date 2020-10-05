a7bbbebd21626356344575e40419ccc9945a6ca9
EdwardHCodes <38704768+EdwardHCodes@users.noreply.github.com>
2020-10-04 22:10:39 -0500

    billy_and_Edwards_user_story_1

--- SSW555Project02FINALVERSION/main.py
+++ SSW555Project02FINALVERSION/main.py
@@ -1,11 +1,6 @@
-
 key_words = ['INDI','NAME','SEX','BIRT','DEAT','FAMC','FAMS','FAM',
 'MARR','HUSB','WIFE','CHIL','DIV','DATE','HEAD','TRLR','NOTE']
-
 levels = ['1','2','3']
-
-
-
 text_file = open('export-BloodTree.ged', 'r')
 
 
@@ -16,10 +11,15 @@ for line in text_file:
   level_number = line[:1]
   
   line = line.split()
+##This is user story to add functionality to print all males in a genealogy
+  if line[1] == "NAME":
+    name = line[2]
+  while line[2] != "M" or "F":
+    if line[2] == "M":
+      print(name)
 
   for word in key_words:
       if word in line:
-       
        derp = word
        y = 'Y'
 
